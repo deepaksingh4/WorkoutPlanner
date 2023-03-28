1809dae8e05ccff5e68c7ace07df57a237be1df2
Deepak Singh <deepak.inbox229@gmail.com>
2023-03-28 12:23:02 +0400

    Initial Commit

---
+++ WorkoutPlanner/WorkoutPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutPlannerApp.swift
+//  WorkoutPlanner
+//
+//  Created by Deepak Singh07 on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
